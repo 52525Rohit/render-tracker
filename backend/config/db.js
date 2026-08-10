647a3e42c1e12aaa1a5a2c83df69171535ab2526
Rohit Kumar <rohitkumarrawani^gmail.com>
2026-08-10 18:17:19 +0530

    we are testing for deployemnet

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,16 +8,26 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  port: Number(process.env.MYSQL_PORT),
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 pool
   .getConnection()
   .then((connection) => {
-    console.log("Connected Successfully");
+    console.log("Database Connected Successfully");
     connection.release();
   })
-  .catch((err) => console.log("Not Connected", err.sqlMessage || err.message));
+  .catch((err) => {
+    console.error("Database Not Connected:", err.message);
+  });
 
 export const query = async (sql, params) => {
   const [rows] = await pool.query(sql, params);
--- frontend/src/utils/context.jsx
+++ frontend/src/utils/context.jsx
@@ -1,1 +1,3 @@
-export const API_END_POINT = "http://localhost:5000";
+//export const API_END_POINT = "http://localhost:5000";
+
+export const API_END_POINT = "https://render-tracker-ws5k.onrender.com";
